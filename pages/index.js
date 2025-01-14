570e343d8951c79439e19bc55b8d7697b6c57e81
Helon21 <helonbentes51@hotmail.com>
2025-01-14 17:29:49 -0300

    primeira tentativa de comunicacao na internet

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home() {
+    return (
+        <div>
+            <h1>Frase para testes na vercel</h1>
+            <div>
+                <h2>Opa bom dia, testa aí pra mim esse negócio, pra eu ver se consigo deixar isso aqui num link totalmente público</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
