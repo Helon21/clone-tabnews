8b94857f3a420fa3174b02225bd298cab4823cf7
Helon21 <helonbentes51@hotmail.com>
2025-01-18 14:30:57 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 function Home() {
-    return (
-        <div>
-            <h1>Frase para testes na vercel</h1>
-            <div>
-                <h2>Opa bom dia, testa aí pra mim esse negócio, pra eu ver se consigo deixar isso aqui num link totalmente público</h2>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Frase para testes na vercel</h1>
+      <div>
+        <h2>
+          Opa bom dia, testa aí pra mim esse negócio, pra eu ver se consigo
+          deixar isso aqui num link totalmente público
+        </h2>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
